3b57da4613393353c36cfa844aa9689d41b92bda
arimitra <arinm209@gmail.com>
2015-10-14 10:42:20 +0100

    JP Morgan Java Exercise
    Java code for calculating stock price

---
+++ src/org/jpmorgan/stocks/service/StocksService.java
@@ -0,0 +1,124 @@
+/**
+ * 
+ */
+package org.jpmorgan.stocks.service;
+
+/**
+ * @author Arindam
+ *
+ */
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.jpmorgan.stocks.StockCalculator;
+import org.jpmorgan.stocks.StockCalculatorFactory;
+import org.jpmorgan.stocks.StocksCalculator;
+import org.jpmorgan.stocks.TradesCalculator;
+import org.jpmorgan.stocks.valueobject.Stock;
+import org.jpmorgan.stocks.valueobject.Trade;
+
+public class StocksService {
+
+	Map<String, Stock> data;  //map symbol -> Stock
+	
+	Map<String, List<Trade>> trades = new HashMap<String, List<Trade>>();
+	/**
+	 * Initialise the the StocksService for performing 
+	 * different stock/trade based operations	
+	 * @param data
+	 */
+	
+	public StocksService(Map<String, Stock> data) {
+		this.data = data;
+	}
+
+/**
+ * Calculate the yield for specific stock
+ * Common Stock : (last div)/(market price)
+ * Preferred Stock : (fixed div)*(Par Value)/(market price)
+ * @param symbol
+ * @return
+ */
+
+	public float calculateDividentYield(String symbol) {
+		Stock stock = data.get(symbol);
+		StockCalculator calculator = StockCalculatorFactory.create(stock.getType());
+		return calculator.calculateYield(stock);
+	}
+
+
+/**
+ * Calculate the PE ratio of a given stock
+ * PE Ratio : (market price)/dividend (use last dividend)
+ * @param symbol
+ * @return
+ */
+	public double calculatePERatio(String symbol) {
+		Stock stock = data.get(symbol);
+		StockCalculator calculator = StockCalculatorFactory.create(stock.getType());
+		return calculator.calculatePERatio(stock);
+	}
+
+/**
+ * In memory recording of a trade based on the ticket
+ * of the trade.
+ * @param trade
+ * @return
+ */
+
+	public int recordTrade(Trade trade) {
+		String tradeTicket = trade.getTicket();
+		List<Trade> stockTrades = getTradesForTicket(tradeTicket);
+		
+		if(null==stockTrades) {
+			stockTrades = new ArrayList<Trade>();
+			trades.put(tradeTicket, stockTrades);
+		}
+		stockTrades.add(trade);
+
+		return 0;
+	}
+
+/**
+ * Get all the trades as a list using a ticket
+ * @param ticket
+ * @return
+ */
+	public List<Trade> getTradesForTicket(String ticket) {
+		return trades.get(ticket);
+	}
+
+/**
+ * Calculate the stock price based on trades that happened 
+ * within a limited period of time. Eg: 15 minutes
+ * @param ticket
+ * @param limit
+ * @return
+ */
+
+	public Stock calculateStockPrice(String ticket, long limit) {
+		List<Trade> trades = this.trades.get(ticket);
+		Stock stock = data.get(ticket);
+		
+		stock.setPrice(new TradesCalculator().calculateStockPrice(ticket,limit,trades));
+		data.put(ticket, stock);
+		
+		return stock;
+	}
+
+/**
+ * Uses utility class to calculate the GBCE for the stocks
+ * @return
+ */
+
+	public double calculateGBCE() {
+		Collection<Stock> stocks = data.values();
+		
+		return new StocksCalculator().calculateGBCE(stocks);
+		
+	}
+}
+
